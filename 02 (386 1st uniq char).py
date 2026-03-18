04ca1ac5d98db1b70e387fa4c95f5a8caa51193e
Truong Giang Do <truonggiang.do01@upol.cz>
2026-03-18 09:02:15 +0100

    386 ✅

--- 02 (386 1st uniq char).py
+++ 02 (386 1st uniq char).py
@@ -1,11 +1,15 @@
-s = "ahojah"
-found = -1
-for i in range(len(s)):
-    if found > -1:
-        break
-    for j in range(i + 1, len(s)):
-        if s[i] == s[j]:
-            break
-    found = i
+s = "ahojahoj"
 
-print("found je", found)
+found = -1                      # pokud nenajdeme unikát, vracíme -1
+counts = {}
+for c in s:                     # projdeme znaky stringu
+    if c in counts:             # vytvoříme slovník
+        counts[c] += 1          # přičteme opakovaný výskyt
+    else:
+        counts[c] = 1           # jinak nastavíme výskyt na 1.
+print(counts)
+for i in range(len(s)):         # projdeme celý slovník
+    if counts[s[i]] == 1:       # hledáme ve slovníku znak na indexu, který se rovná 1
+        found = i
+        break                   # ukončíme předčasně cyklus
+print(found)

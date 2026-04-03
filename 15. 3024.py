beb58af7cef133e4e54eeaf9e436c598d96bc2b9
Truong Giang Do <truonggiang.do01@upol.cz>
2026-04-03 22:35:49 +0200

    3024 ✅

--- 15. 3024.py
+++ 15. 3024.py
@@ -6,13 +6,13 @@ nums = [5,3,8]
 
 def tria(nums):
     if ((nums[0] + nums[1] > nums[2]) and
-        (nums[0] + nums[1] > nums[1]) and
+        (nums[0] + nums[2] > nums[1]) and
         (nums[1] + nums[2] > nums[0])):
         return("equilateral" if (nums[0] == nums[1] == nums[2]) else
                ("isosceles" if ((nums[0] == nums[1]) or 
                                 (nums[1] == nums[2]) or 
                                 nums[0] == nums[2]) else "scalene"))
     else:
-        return "Null"
+        return "none"
     
 print(tria(nums))
